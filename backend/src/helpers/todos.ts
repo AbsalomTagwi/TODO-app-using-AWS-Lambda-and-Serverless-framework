9be8b48061e1971f593a67b89f99df353aa53439
AbsalomTagwi <absalomtagwi@gmail.com>
2022-08-10 18:37:58 +0200

    Final commit

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,48 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { todosAcess } from './todosAcess'
+//import { attachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
-import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+//import { createLogger } from '../utils/logger'
+//import * as uuid from 'uuid'
+//import * as createError from 'http-errors'
+import {parseUserId} from "../auth/utils";
+import {TodoUpdate} from "../models/TodoUpdate";
 
 // TODO: Implement businessLogic
+const uuidv4 = require('uuid/v4');
+const toDoAccess = new todosAcess();
+
+export async function getTodosForUser(jwtToken: string): Promise<TodoItem[]> {
+    const userId = parseUserId(jwtToken);
+    return toDoAccess.getTodosForUser(userId);
+}
+
+export function createTodo(createTodoRequest: CreateTodoRequest, jwtToken: string): Promise<TodoItem> {
+    const userId = parseUserId(jwtToken);
+    const todoId =  uuidv4();
+    const s3BucketName = process.env.S3_BUCKET_NAME;
+    
+    return toDoAccess.createTodo({
+        userId: userId,
+        todoId: todoId,
+        attachmentUrl:  `https://${s3BucketName}.s3.amazonaws.com/${todoId}`, 
+        createdAt: new Date().getTime().toString(),
+        done: false,
+        ...createTodoRequest,
+    });
+}
+
+export function updateTodo(updateTodoRequest: UpdateTodoRequest, todoId: string, jwtToken: string): Promise<TodoUpdate> {
+    const userId = parseUserId(jwtToken);
+    return toDoAccess.updateTodo(updateTodoRequest, todoId, userId);
+}
+
+export function deleteTodo(todoId: string, jwtToken: string): Promise<string> {
+    const userId = parseUserId(jwtToken);
+    return toDoAccess.deleteTodo(todoId, userId);
+}
+
+export function createAttachmentPresignedUrl(todoId: string): Promise<string> {
+    return toDoAccess.createAttachmentPresignedUrl(todoId);
+}
